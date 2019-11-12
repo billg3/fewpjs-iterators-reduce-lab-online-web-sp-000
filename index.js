601d7f0c04df07302840394ff7e1389c96d3fce0
Bill G <gbilly3@gmail.com>
2019-11-12 01:12:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // Code your solution here
 
 
-batteryBatches.reduce ( totalBatteries, number ) => number + totalBatteries, 0)  
-
-
+batteryBatches.reduce ( totalBatteries, number ) => number + totalBatteries, 0)
